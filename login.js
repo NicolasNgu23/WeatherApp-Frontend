2a20ed3d0067805c317f22a5d18b12bdf1efbc76
NicolasNgu23 <nicolasnguyen0023@hotmail.com>
2024-02-22 16:06:26 +0100

    update fetch

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ document.querySelector('#register').addEventListener('click', function () {
 		password: document.querySelector('#registerPassword').value,
 	}
 
-	fetch('http://localhost:3000/users/signup', {
+	fetch('https://weather-app-backend-gray-nine.vercel.app/users/signup', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(user),
@@ -26,7 +26,7 @@ document.querySelector('#connection').addEventListener('click', function () {
 		password: document.querySelector('#connectionPassword').value,
 	}
 
-	fetch('http://localhost:3000/users/signin', {
+	fetch('https://weather-app-backend-gray-nine.vercel.app/users/signin', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(user),
